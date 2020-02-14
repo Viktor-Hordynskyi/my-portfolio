4f8c5cd751b150e73f9f132a438cbe7833521b93
Viktor <work.morpi@gmail.com>
2020-02-14 09:59:41 +0200

    path fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/build/')));
+app.use(express.static(path.join(__dirname, 'client/build/')));
 app.use(bodyParser.json());
 
 app.use('/', indexRouter);
