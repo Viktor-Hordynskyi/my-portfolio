befb034d9298c7a489dfe93c1797d1f83772930b
Viktor <work.morpi@gmail.com>
2020-02-14 10:35:20 +0200

    some fixes

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const { getAllSkills, getAllProjects } = require("../controllers/skills");
 const { nodemailer } = require("../controllers/nodemailer");
 
 // Basic "/index" routes
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '../controllers/nodemailer', 'index.html'));
+router.get("/", (req, res) => {  
   res.status(200).send([{ skills: "/skills" }, { projects: "/projects" }]);
+  res.sendFile(path.join(__dirname, '../controllers/nodemailer', 'index.html'));
 });
 
 /* GET pages. */
